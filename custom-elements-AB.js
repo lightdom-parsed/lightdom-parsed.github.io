83924f07d587f09fe9fd9ab612f86a77ec18765d
Danny Engelman <danny@engelman.nl>
2025-04-22 16:56:35 +0200

    v0.1 sneak preview

---
+++ custom-elements-AB.js
@@ -0,0 +1,126 @@
+
+// **************************************************************************** urlParams
+const urlParams = new URLSearchParams(window.location.search);
+// **************************************************************************** createElement
+const createElement = (
+  /* string */ tag,
+  /* Object */ options = {},
+  /* Node   */ element = document.createElement(tag) // create or use existing element
+) => {
+    //console.error(`%c createElement <${tag}> `, "background:blue;color:white", this);
+    const {
+        prepend = [], // prepend at start
+        append = [], // append at end
+        classes = [], // set .classList
+        styles = {}, // set .style
+        attrs = {}, // set attributes
+        ...props // catch remaining props, like listeners onclick, className, etc
+    } = options;
+    element.prepend(...prepend); // prepend children
+    element.append(...append); // append children
+    element.classList.add(...classes); // add classes
+    Object.keys(attrs).forEach((key) => element.setAttribute(key, attrs[key])); // set attributes
+    Object.assign(element.style, styles); // assign style
+    return Object.assign(element, props); // assign remaining properties
+}
+const createTag = (tag, innerHTML, props = {}) => createElement(tag, { innerHTML, ...props })
+
+// **************************************************************************** define <my-custom-element> A and B
+function defineCustomElementsAandB() {
+    let linenr = 1
+    customElements.define(
+        "my-custom-element",
+        class extends HTMLParsedElement {
+            // ================================================================ log
+            log(...attrs) {
+                let componentID = this.id || "B"; // no attributes yet on component B when constructor is called!
+                let methodName = attrs.shift();
+                let isConnected = this.isConnected;
+                let has_lightDOM = this.textContent.length > 0;
+                let explanationID = "explanations #" + componentID.split(" ")[0] + methodName;
+                setTimeout(() => { // timeout ensure innerHTML is available to log
+                    document.body.appendChild(
+                        this.details = createElement("details", {
+                            className: "component" + componentID,
+                            append: [
+                                createElement("summary", {
+                                    append: [
+                                        createTag("tag-linenr", linenr++),
+                                        createTag("tag-methodname", methodName.split("__")[0]),
+                                        createTag("tag-isconnected", (isConnected ? "TRUE" : "FALSE")),
+                                        createTag("tag-lightdom", (has_lightDOM ? "" : "NOT") + " available"),
+                                        createTag("tag-attrschanged", attrs),
+                                    ]
+                                }),
+                                createElement("tag-explanation", {
+                                    append: [
+                                        document.querySelector(explanationID)?.cloneNode(true), // lightDOM gets slotted
+                                    ]
+                                })
+                            ], // append
+                            onclick: (e) => {
+                                e.preventDefault();
+                                let details = e.target.closest("details");
+                                details.toggleAttribute("open", !details.hasAttribute("open"));
+                            },
+                        }) // details
+                    ).toggleAttribute("open", urlParams.has('open')); // initial open state
+                })
+            }
+            // ================================================================ constructor
+            constructor() {
+                super().log("constructor")
+            }
+            // ================================================================ observedAttributes
+            static get observedAttributes() {
+                return ["attr1", "attr2"]
+            }
+            // ================================================================ attributeChangedCallback
+            attributeChangedCallback(name, oldValue, newValue) {
+                this.log(`attributeChangedCallback__${name}`, `${name},${oldValue},${newValue}`)
+            }
+            // ================================================================ connectedCallback
+            async connectedCallback() {
+                this.log(`connectedCallback`)
+                if (!this.isConnected) {
+                    this.log("not connected in connectedCallback")
+                    return
+                }
+                //this.render()
+                requestAnimationFrame(() => {
+                    let title = "rAF_triggered"
+                    if (this.setTimeout_triggered) {
+                        title = "rAF_triggered after setTimeout"
+                    }
+                    this.log(title)
+                    this.addHTML("rAF");
+                })
+                setTimeout(() => {
+                    this.setTimeout_triggered = true;
+                    this.log("setTimeout_triggered")
+                    this.addHTML("setTimeout");
+                })
+                setTimeout(() => {
+                    this.setAttribute("attr2", "BAR")
+                })
+                await Promise.resolve()
+                this.log("Promise_resolved")
+            }
+            // ================================================================ addHTML
+            addHTML(method = "") {
+                return
+                document.body.append(
+                    createElement("h3", {
+                        innerHTML: `Component <b>${this.id}</b> : add HTML in ` + method,
+                        className: "DOMevent"
+                    }),
+                )
+            }
+            // ================================================================ parsedCallback
+            parsedCallback() {
+                this.log("parsedCallback")
+            }
+
+        }, // class extends HTMLParsedElement
+    ) // customElements.define("my-custom-element"
+} // defineCustomElementsAandB
---
+++ tags-display.js
@@ -0,0 +1,156 @@
+// Hierarchy: 
+// HTMLElement -> TagBaseClass -> TagLabelClass -> tag-name
+
+// **************************************************************************** TagBaseClass
+// BaseClass for all custom elements, to attach shadowDOM and CSS/HTML
+class TagBaseClass extends HTMLElement {
+    connectedCallback(html = this.html) {
+        this.attachShadow({
+            mode: "open"
+        }).innerHTML = `<style>:host{display:inline-block}</style>` + html
+    }
+}
+// **************************************************************************** <tag-name>
+// display tag-name in blue as <tag-name>
+customElements.define("tag-name", class extends TagBaseClass {
+    html = `<style>:host{color:blue}</style>` +
+        `<<slot></slot>>`
+});
+// **************************************************************************** <tag-attr>
+// display tag-attr in black as name="value"
+customElements.define("tag-attr", class extends TagBaseClass {
+    html = `<style>` +
+        `:host{color:var(--attrhighlight)}` +
+        `span{color:black}` +
+        `is{color:grey}` +
+        `slot{font-weight:bold}` +
+        `</style>` +
+        `<span>${this.getAttribute("name")}</span><is>=</is>"` +
+        `<slot></slot>"`
+});
+// **************************************************************************** <tag-script>
+// display tag-script in black as <script>...</script>
+customElements.define("tag-script", class extends TagBaseClass {
+    html = `<style>` +
+        `:host{margin:.3em;font-size:140%}` +
+        `</style>` +
+        `<tag-name>script</tag-name>` +
+        `<slot></slot>` +
+        `<tag-name>script</tag-name>`;
+})
+// **************************************************************************** class
+class TagLabelClass extends TagBaseClass {
+    connectedCallback() {
+        this.init && this.init()
+        let [background = "red", color = "black"] = (this.colors || "").split(",")
+        super.connectedCallback(`<style>` +
+            `:host{display:inline-block;background:${background};color:${color};padding:0 4px;` +
+            //(this.style || "") +
+            `}` +
+            (this.css || "") +
+            `</style>` +
+            (this.prefix || "") +
+            `<slot></slot>`)
+    }
+}
+// **************************************************************************** <tag-methodname>
+// display tag-methodname in black as methodname
+customElements.define("tag-methodname", class extends TagLabelClass {
+    init() {
+        const methodName = this.innerHTML.trim();
+        const defaultmethods = ["constructor", "connectedCallback", "disconnectedCallback", "adoptedCallback", "attributeChangedCallback"];
+        const isDefault = defaultmethods.includes(methodName);
+        const isTrigger = methodName.includes("trigger");
+        const isAfter = methodName.includes("after");
+
+        if (isDefault) {
+            this.colors = "green,beige";
+        } else if (isTrigger) {
+            if (isAfter) {
+                RAFLATE.style.display = "block";
+                RAFEARLY.style.display = "none";
+                this.colors = "orangered,beige";
+            } else {
+                RAFLATE.style.display = "none";
+                RAFEARLY.style.display = "block";
+                this.colors = "gold";
+            }
+        }
+    }
+});
+// **************************************************************************** <tag-isconnected>
+// display tag-isconnected in black as isConnected=true
+customElements.define("tag-isconnected", class extends TagLabelClass {
+    init() {
+        this.colors = this.innerHTML.includes("TRUE") ? "lightgreen" : "var(--warningcolor),beige";
+    }
+    prefix = `<label>this.isConnected=</label>`
+});
+// **************************************************************************** <tag-linenr>
+// display tag-linenr in black as #linenr
+customElements.define("tag-linenr", class extends TagLabelClass {
+    colors = "none";
+    css = `:host{text-align:center}`;
+    prefix = `<label>#</label>`
+});
+// **************************************************************************** <tag-lightdom>
+// display tag-lightdom in black as lightDOM available
+customElements.define("tag-lightdom", class extends TagLabelClass {
+    prefix = `<label>lightDOM </label>`;
+    connectedCallback() {
+        if (this.innerHTML.includes("NOT")) {
+            this.colors = "var(--warningcolor),beige";
+        } else {
+            this.colors = "lightgreen";
+        }
+        super.connectedCallback();
+    }
+});
+// **************************************************************************** <tag-attrs>
+// display tag-attrs HTML "name,oldValue,newValue" as name="value" ...
+customElements.define("tag-attrschanged", class extends TagBaseClass {
+    connectedCallback() {
+        let [name, oldValue, newValue] = this.innerHTML.split(",");
+        let html = (label, str) => label + `=<b>${str == "null" ? `<span>${str}</span>` : str}</b>`;
+        this.innerHTML = "";// clear lightDOM
+        if (name) {
+            super.connectedCallback(`<style>` +
+                `:host{grid-column:2/span 3}` +
+                `span{color:var(--warningcolor,firebrick)}` +
+                `</style>` +
+                [html("name", name), html("oldValue", oldValue), html("newValue", newValue)].join(" - "));
+        }
+    }
+})
+// **************************************************************************** <tag-explanation>
+// display tag-explanation in black as <tag-explanation>...</tag-explanation>
+customElements.define("tag-explanation", class extends TagBaseClass {
+    style = `:host{display:inline-block;margin:3px 0 0 42px;font-size:var(--explanationFontSize)}` +
+        `:host{grid-column:2/span 3}`;
+    html = `<slot part="explanation"></slot>`
+})
+// **************************************************************************** <component-html>
+// display Component A and B as visible HTML
+customElements.define(
+    "component-html",
+    class extends TagBaseClass {
+        connectedCallback() {
+            let id = this.getAttribute("component");
+            this.classList.add("component" + id)
+            // ---------------------------------------------------------------- HTML
+            let tagname = `<tag-name>my-custom-element</tag-name>`;
+            let attr = (name, value) => `<tag-attr name="${name}">${value}</tag-attr>`;
+
+            this.html = `<style>` +
+                `:host{display:block;background:var(--h1background)}` +
+                `h{display:block;font-size:140%;padding-bottom:.2em}` +
+                `.description{font-size:140%}` +
+                `.highlight{font-size:100%;color:var(--attrhighlight);font-weight:bold}` +
+                `</style>` +
+                `<h>${tagname} ${attr("id", id)} ${attr("attr1", "foo")}>lightDOM${tagname}></h>` +
+                `<span class="description">is in the DOM, <span class="highlight">${id == "A" ? "BEFORE" : "AFTER"}</span> Element ${id} is defined with script</span>`
+
+            super.connectedCallback();
+        }
+    },
+)
